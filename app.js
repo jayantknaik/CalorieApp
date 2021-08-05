5864b4cbc4f3edb7270b4bec3dc8004c17ff2f14
Jayant Kumar Naik <jayantnaik98@gmail.com>
2021-08-05 15:48:37 +0530

    update

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const { Tdee } = require("./Backend/database/model/tdeeModel");
 const port = process.env.PORT || 3000;              
 app.use(express.json());
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:4200"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "https://calorie-app-heroku.herokuapp.com"); // update to match the domain you will make the request from
   res.header(
     "Access-Control-Allow-Methods",
     "GET, POST, HEAD, OPTIONS, PUT, PATCH, DELETE"
